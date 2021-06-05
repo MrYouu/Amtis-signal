9480fdee455d29aff1102b41a056e709b8a32308
MrYouu <krisvalchev@gmail.com>
2021-06-05 16:23:28 +0300

    Done

--- Files/Functions.js
+++ Files/Functions.js
@@ -13,3 +13,41 @@ class loadSmallToBigImage {
     }
 }
 
+class mouseClickAndDrag {
+    constructor(elementID) {
+        this.elementToScroll = document.getElementById(elementID);
+        this.elementToScrollPosition = { top: 0, left: 0, x: 0, y: 0 };
+
+        elementToScroll.addEventListener("mousedown", this.elementToScrollMouseDownHandler);
+    }
+
+    elementToScrollMouseDownHandler = function (e) {
+        this.elementToScroll.style.userSelect = "none";
+
+        this.elementToScrollPosition = {
+            left: this.elementToScroll.scrollLeft,
+            top: this.elementToScroll.scrollTop,
+
+            x: e.clientX,
+            y: e.clientY,
+        };
+
+        document.addEventListener("mousemove", this.elementToScrollMouseMoveHandler);
+        document.addEventListener("mouseup", this.elementToScrollMouseUpHandler);
+    };
+
+    elementToScrollMouseMoveHandler = function (e) {
+        const dx = e.clientX - this.elementToScrollPosition.x;
+        const dy = e.clientY - this.elementToScrollPosition.y;
+
+        this.elementToScroll.scrollTop = this.elementToScrollPosition.top - dy;
+        this.elementToScroll.scrollLeft = this.elementToScrollPosition.left - dx;
+    };
+
+    elementToScrollMouseUpHandler = function () {
+        this.elementToScroll.style.removeProperty("user-select");
+
+        document.removeEventListener("mousemove", this.elementToScrollMouseMoveHandler);
+        document.removeEventListener("mouseup", this.elementToScrollMouseUpHandler);
+    };
+}
